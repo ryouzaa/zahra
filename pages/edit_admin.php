ad8e7a2903b9bec82af2f58cf272fe1cbadc6ba8
rifkapratika00 <rifka.pratika@students.utdi.ac.id>
2023-01-12 05:04:44 +0700

    upload

--- pages/edit_admin.php
+++ pages/edit_admin.php
@@ -1,44 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">DASHBOARD</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="?hal=admin">Admin</a>
-        <a class="nav-link" href="#">Paket Menu</a>
-        <a class="nav-link" href="#">Reservasi</a>
-        <a class="nav-link" href="#">Tempat</a>
-      </div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">DASHBOARD</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="?hal=admin">Admin</a>
+                    <a class="nav-link" href="menu.php">Paket Menu</a>
+                    <a class="nav-link" href="#">Reservasi</a>
+                    <a class="nav-link" href="tempat.php">Tempat</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <h2 class="text-center" style="margin-top:25px;">Edit Data Admin</h2>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Kode Admin</label>
+                <input type="text" class="form-control" name="Kd_Admin" id="formGroupExampleInput"
+                    placeholder="Example input placeholder">
+            </div>
+            <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Nama Admin</label>
+                <input type="text" class="form-control" name="Nm_Admin" id="formGroupExampleInput2"
+                    placeholder="Another input placeholder">
+            </div>
+            <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">No Telepon</label>
+                <input type="text" class="form-control" name="No_Tlp" id="formGroupExampleInput2"
+                    placeholder="Masukan No">
+            </div>
+
+            <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Email</label>
+                <input type="text" class="form-control" name="Email" id="formGroupExampleInput2"
+                    placeholder="Nama Email">
+            </div>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+            </div>
+        </form>
     </div>
-  </div>
-</nav>
-<div class="container">
-<h2 class="text-center" style="margin-top:25px;">Edit Admin</h2>
-<form action="" method="post">
-<div class="mb-3">
-  <label for="formGroupExampleInput" class="form-label">Example label</label>
-  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
-</div>
-<div class="mb-3">
-  <label for="formGroupExampleInput2" class="form-label">Another label</label>
-  <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
-</div>
-<div class="mb-3">
-    <button type="submit" class="btn btn-primary">Submit</button>
-</div>
-</form>
-</div>
 </body>
+
 </html>
--- pages/edit_menu.php
+++ pages/edit_menu.php
@@ -1,49 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">DASHBOARD</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="?hal=admin">Admin</a>
-        <a class="nav-link" href="#">Paket Menu</a>
-        <a class="nav-link" href="#">Reservasi</a>
-        <a class="nav-link" href="#">Tempat</a>
-      </div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">DASHBOARD</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="?hal=admin">Admin</a>
+                    <a class="nav-link" href="menu.php">Paket Menu</a>
+                    <a class="nav-link" href="">Reservasi</a>
+                    <a class="nav-link" href="tempat.php">Tempat</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <h2 class="text-center" style="margin-top:25px;">Data Edit Menu</h2>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Kode Paket Menu</label>
+                <input type="text" class="form-control" name="Kd_Paket_Menu" id="formGroupExampleInput"
+                    placeholder="Example input placeholder">
+            </div>
+            <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Nama Paket Menu</label>
+                <input type="text" class="form-control" name="Nm_Paket_Menu" id="formGroupExampleInput"
+                    placeholder="Example input placeholder">
+            </div>
+            <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Harga Paket</label>
+                <input type="text" class="form-control" name="Harga_Paket" id="formGroupExampleInput2"
+                    placeholder="Another input placeholder">
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                </div>
+        </form>
     </div>
-  </div>
-</nav>
-<div class="container">
-<h2 class="text-center" style="margin-top:25px;">Data Admin</h2>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>EDIT | DELETE</td>
-    </tr>
-  </tbody>
-</table>
-</div>
 </body>
+
 </html>
