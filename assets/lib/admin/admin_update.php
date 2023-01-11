99839771a4e1d97f671e6a440be9329a60aa48b0
faisal.najib <faisal.najib@digitalsekuriti.id>
2023-01-12 03:28:52 +0700

    add file

---
+++ assets/lib/admin/admin_update.php
@@ -0,0 +1,13 @@
+<?php 
+
+include "../conn.php";
+
+$id = $_POST['id'];
+
+$sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . mysqli_error($conn);
+}
